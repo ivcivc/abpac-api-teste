07a652713fda2d48b1686c7816cbd4d74d6b27f2
ivan.a.oliveira@terra.com.br <ivan.a.oliveira@terra.com.br>
2019-08-27 13:47:44 -0300

    ws

--- start/routes.js
+++ start/routes.js
@@ -14,13 +14,12 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-/*const Route = use("Route");
+const Route = use("Route");
 
 Route.group(() => {
   Route.get("/", () => {
-    return { greeting: "Hello world in JSON....." };
+    return { greeting: "Adonis - Hello world in JSON....." };
   });
 }).prefix("api");
-*/
-//++++***
 
+//++++***
