925b6b3c4b3a1c321b6af1a4480656a168ffe54c
ivan.a.oliveira@terra.com.br <ivan.a.oliveira@terra.com.br>
2022-06-13 07:23:39 -0300

    Correção coloração no status cancelamento  beneficios

--- database/migrations/1573481607313_equipamento_beneficio_status_schema.js
+++ database/migrations/1573481607313_equipamento_beneficio_status_schema.js
@@ -4,39 +4,39 @@
 const Schema = use('Schema')
 
 class EquipamentoBeneficioStatusSchema extends Schema {
-  up () {
-    this.create('equipamento_beneficio_statuses', (table) => {
-      table.increments()
+	up() {
+		this.create('equipamento_beneficio_statuses', table => {
+			table.increments()
 
-      table.varchar("motivo", 250);
+			table.varchar('motivo', 250)
 
-      table
-      .enu("status", ["Ativo", "Inativo"], {
-          useNative: true,
-          existingType: true,
-          enumName: "equipamento_beneficio_status"
-        })
-        .notNullable()
-        .defaultTo("Ativo");
+			table
+				.enu('status', ['Ativo', 'Inativo', 'Cancelado'], {
+					useNative: true,
+					existingType: true,
+					enumName: 'equipamento_beneficio_status',
+				})
+				.notNullable()
+				.defaultTo('Ativo')
 
-      table
-        .integer('equipamento_beneficio_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('equipamento_beneficios')
-        .onUpdate('CASCADE')
-        .onDelete('RESTRICT')
+			table
+				.integer('equipamento_beneficio_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('equipamento_beneficios')
+				.onUpdate('CASCADE')
+				.onDelete('RESTRICT')
 
-      table.integer('user_id').notNullable()
+			table.integer('user_id').notNullable()
 
-      table.timestamps()
-    })
-  }
+			table.timestamps()
+		})
+	}
 
-  down () {
-    this.drop('equipamento_beneficio_statuses')
-  }
+	down() {
+		this.drop('equipamento_beneficio_statuses')
+	}
 }
 
 module.exports = EquipamentoBeneficioStatusSchema
