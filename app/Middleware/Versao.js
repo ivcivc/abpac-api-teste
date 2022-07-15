4afa589907a9e9a81bc66ea735d55126d5562b88
ivan.a.oliveira@terra.com.br <ivan.a.oliveira@terra.com.br>
2022-07-14 23:10:09 -0300

    Fix

--- app/Middleware/Versao.js
+++ app/Middleware/Versao.js
@@ -13,7 +13,7 @@ class Versao {
 		// call next to advance the request
 
 		const headers = request.headers()
-		if (headers.versao !== '1.52') {
+		if (headers.versao !== '1.53') {
 			return response
 				.status(404)
 				.send(
